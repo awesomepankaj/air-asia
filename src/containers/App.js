d6c289c53526541f55b552d341bb0acce39e0dc2
Suraj Kumar <122suraj0@gmail.com>
2018-03-20 10:49:02 +0530

    intial commit

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './../stylesheet/App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+	      <h1>Hello Air Asia</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
