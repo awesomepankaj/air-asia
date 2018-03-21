c1e070f8699f4133343b29d99abf7f6963a53c04
Suraj Kumar <122suraj0@gmail.com>
2018-03-21 12:53:53 +0530

    add comment btn in comment layout

--- src/components/UserComment.js
+++ src/components/UserComment.js
@@ -7,6 +7,10 @@ class UserComment extends Component {
 				<div className="feedback__block container">
 					<p>Please leave your valuable comments</p>
 					<textarea id="input" className="form-control comment" rows="3" placeholder="Please add your comment here" />
+					<div className="comment__btn">
+						<button type="button" className="btn btn-primary">Done</button>
+						<span>Skip</span>
+					</div>
 				</div>
 			</div>
 		);
